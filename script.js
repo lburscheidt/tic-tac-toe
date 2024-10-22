8ca7aaea96de263d07006d110c10d188057fb48f
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-22 14:04:25 +0200

    feat: add win/draw logic to browser board

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
-// Player objects
-
-function Player(name, marker) {
-  this.name = name;
-  this.marker = marker;
-  this.move = function (position) {
-    console.clear();
-    Gameboard[position - 1] = marker;
-    console.log(Gameboard);
-    screenBoard();
-  };
-}
-
-const playerOne = new Player("steve", "X");
-const playerTwo = new Player("tony", "O");
+//Global code
+let board = document.querySelector(".board");
 
 //Gameboard object
 const Gameboard = (function () {
-  //  return [
-  //    [7, 8, 9],
-  //    [4, 5, 6],
-  //    [1, 2, 3],
-  //  ];
-  return [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+return gameboard
 })();
 
-console.table(Gameboard);
-let board = document.querySelector(".board");
 function screenBoard() {
   board.innerHTML = "";
   Gameboard.map((element) => {
@@ -36,20 +17,38 @@ function screenBoard() {
   });
 }
 
-screenBoard();
+window.addEventListener("load", function (){ console.table(Gameboard); screenBoard()})
+
+// Player objects
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+this.move = function (position) {
+  console.clear(); 
+Gameboard[position - 1] = marker;
+ console.log(Gameboard)
+  screenBoard();
+  };}
+
+
+const playerOne = new Player("steve", "X");
+const playerTwo = new Player("tony", "O");
+
 const playGame = (function () {
+
+ const hasWon = function () {
   let g = Gameboard;
-  const _1 = g[0];
-  const _2 = g[1];
-  const _3 = g[2];
-  const _4 = g[3];
-  const _5 = g[4];
-  const _6 = g[5];
-  const _7 = g[6];
-  const _8 = g[7];
-  const _9 = g[8];
-  const hasWon = function () {
-    let m = currentPlayer.marker;
+let m = currentPlayer.marker;
+  let _1 = g[0];
+  let _2 = g[1];
+  let _3 = g[2];
+  let _4 = g[3];
+  let _5 = g[4];
+  let _6 = g[5];
+  let _7 = g[6];
+  let _8 = g[7];
+  let _9 = g[8];
+
     if (
       (_9 === m && _8 === m && _7 === m) ||
       (_6 === m && _5 === m && _4 === m) ||
@@ -68,7 +67,18 @@ const playGame = (function () {
   };
 
   const isDraw = function () {
-    let m = currentPlayer.marker;
+    let g = Gameboard;
+let m = currentPlayer.marker;
+  let _1 = g[0];
+  let _2 = g[1];
+  let _3 = g[2];
+  let _4 = g[3];
+  let _5 = g[4];
+  let _6 = g[5];
+  let _7 = g[6];
+  let _8 = g[7];
+  let _9 = g[8];
+
     if (
       (_2 === m && _4 === m && _6 === m && _7 === m && _9 === m) ||
       (_1 === m && _2 === m && _6 === m && _7 === m && _8 === m) ||
@@ -93,40 +103,23 @@ const playGame = (function () {
       return false;
     }
   };
+
+
   let currentPlayer = playerOne;
 
-  board.addEventListener("click", (e) => {
-    let field = e.target;
-    field.textContent = currentPlayer.marker;
-    console.log(Gameboard);
+  board.addEventListener("click", (e) => {currentPlayer.move(e.target.id);
     if (hasWon()) {
       alert(hasWon());
+
     }
-    if (isDraw()) {
+     if (isDraw()) {
       alert(isDraw());
     } else {
       if (currentPlayer == playerOne) {
         currentPlayer = playerTwo;
       } else {
-        currentPlayer = playerOne;
-      }
-    }
-  });
-  //
-  //  window.addEventListener("click", function () {
-  //    currentPlayer.move(prompt(`${currentPlayer.name}, please select x`));
-  //    console.log(Gameboard);
-  //    if (hasWon()) {
-  //      alert(hasWon());
-  //    }
-  //    if (isDraw()) {
-  //      alert(isDraw());
-  //    } else {
-  //      if (currentPlayer == playerOne) {
-  //        currentPlayer = playerTwo;
-  //      } else {
-  //        currentPlayer = playerOne;
-  //      }
-  //    }
-  //  });
-})();
+        currentPlayer = playerOne;}}})
+
+});
+
+playGame();
